6719995e81dbc33dc2b5104ab0f2498d31e57bc6
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2025-04-03 13:20:33 +0000

    Thursday work all I could do/finished

--- contactManager.py
+++ contactManager.py
@@ -2,12 +2,11 @@
 contacts = {} #works in 1 and 2, just not printing keys & values in 2
 
 #Option 1 to add new contact
-def add_contact(): #works so far
+def add_contact():
     name = input("Please enter the contact's name: ")
     contacts[name] = {}
 
     phone_num = input("Please enter the contact's phone number: ")
-    # contacts[name] = phone_num #think mess up is here
 
     email = input("Please enter the contact's email: ")
     contacts[name][phone_num] = email
@@ -16,32 +15,41 @@ def add_contact(): #works so far
     return contacts
 
 # Function to view all contacts
-def view_contacts(): # <-- no parameter? works but two dif lines
+def view_contacts(): 
     print("=== All Contacts ===")
     for name, contacts_list in contacts.items():
-        print(f"{name}:")
         for phone_num, email in contacts_list.items():
             print("")
-            print(f"Phone - {phone_num} Email - {email}")
-
-# if len == 0:
-    # print("No contacts found")
+            print(f"{name} : Phone - {phone_num}, Email - {email}")
 
 # Function to search for a contact by name
-def search_name():
+def search_contact():
     name = input("Enter the name of the contact to search for:")
     if name in contacts:
-      print(f"Contact found:{name} : Phone - {phone_num}, Email -") # same issue as viewing, keys/values not being called
+      print(f"Contact found:{name} : Phone - {phone_num}, Email - {email}") #?
     elif name not in contacts:
-       print("Countact not found.")
+       print("Contact not found.")
+    else:
+        print("Error")
 
 # Function to remove a contact 
-# #del method
-#same as above for not in contacts
+def remove_contact():
+    name = input("Enter the name of the contact you would like to delete")
+    if name in contacts:
+        del contacts[name]
+    elif name not in contacts:
+        print("Contact not found.")
+    else:
+        print("Error")
 
 # Function to update a contact's information 
-#.append?
-#sane as above for not in contacts
+def update_contact(): 
+    name = input("Please input the name of the contact you would like to update")
+    if name in contacts:
+        phone_num = input("Please input the number of the contact")
+        [name].append(phone_num)
+        email = input("Please input the email of the contact")
+        [name].append(email)
 
 # Main function to run the menu-driven system
 def main():
@@ -61,10 +69,10 @@ def main():
             view_contacts()
         elif choice == 3:
             search_contact()
-        # elif choice == 4:
-        #     remove_contact()
-        # elif choice == 5:
-        #     update_contact
+        elif choice == 4:
+            remove_contact()
+        elif choice == 5:
+            update_contact()
         elif choice == 6:
             print("Goodbye!")
             break
